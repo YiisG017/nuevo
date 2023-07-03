70b7256fea54a9f72ca957a1945f01d5f4e8d47f
Viridiana Leal <lerv060816mhglmra8@soycecytem.mx>
2023-07-03 08:19:59 -0500

    holiii

---
+++ TablasMultiplicar/main.js
@@ -0,0 +1,14 @@
+const generar=()=>{
+    let n = parseInt(document.querySelector("#n").value)
+     document.querySelector("#vn").innerHTML=n
+    document.querySelector("#res").innerHTML 
+     let div=document.querySelector("#res");
+     let tablaHTML="<table>";
+     for(i=1;i<=10; i++){
+       tablaHTML+="<tr><td>"+n+"x"+i+"="+(i*n)+"</td></tr>";
+     }
+     div.innerHTML=tablaHTML+"</table>";
+    }
+    const borrar=()=>{
+      document.querySelector("#res").innerHTML="";
+    }
---
+++ cambiarImagenes/main.js
@@ -0,0 +1,13 @@
+
+const uno=()=>{
+  let img=document.querySelector("#imagen");
+  img.src="pp.jpg";
+}
+const dos=()=>{
+  let img=document.querySelector("#imagen");
+  img.src="helloK.jpg";
+}
+const tres=()=>{
+  let img=document.querySelector("#imagen");
+  img.src="jh.jpg";
+}
